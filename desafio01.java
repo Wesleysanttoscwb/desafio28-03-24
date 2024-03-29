766b25e8869803e9a42ba1c804672b93d1d0e6be
Wesleysanttoscwb <162758399+Wesleysanttoscwb@users.noreply.github.com>
2024-03-29 09:31:20 -0300

    desafio01.java

---
+++ desafio01.java
@@ -0,0 +1,14 @@
+package Aula09;
+import java.util.Scanner;
+
+public class Desafio01 {
+	public static void main(String[] args) {
+		String nome = "Wesley Santos Silva";
+		
+		String primeiroNome = nome.substring(0,6);
+		
+		System.out.println(primeiroNome);
+	}
+	
+
+}
