a6c7812c00519a6d8d8352e1abdad2890faca2d8
Wesleysanttoscwb <162758399+Wesleysanttoscwb@users.noreply.github.com>
2024-03-29 09:48:30 -0300

    desafio02.java

---
+++ desafio02.java
@@ -0,0 +1,16 @@
+package Aula09;
+import java.util.Scanner;
+
+public class Desafio02 {
+	public static void main(String[] args) {
+		String nome = "Wesley Santos Silva";
+		
+		String primeiroNome = nome.substring(0,6);
+		
+		String maiuscula = primeiroNome.toUpperCase();
+		
+		System.out.println(maiuscula);
+	}
+	
+
+}
