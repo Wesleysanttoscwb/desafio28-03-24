958076eba82509e41307ce474eed84131fc6e10c
Wesleysanttoscwb <162758399+Wesleysanttoscwb@users.noreply.github.com>
2024-03-29 14:55:41 -0300

    Create desafio7B.java

---
+++ desafio7B.java
@@ -0,0 +1,11 @@
+package Aula09;
+
+public class Desafio7b {
+	public static void main(String [] args) {
+		String conteudo = "  teste 123  ";
+		String nova = conteudo.trim();
+		
+		System.out.println(nova);
+	}
+
+}
